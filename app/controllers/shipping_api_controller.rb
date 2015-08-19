7c2418293db4ea6f05761c8e2559326d84adfa4c
Ashley Watkins <AWatkins31@gmail.com>
2015-08-19 14:06:33 -0700

    WIP -- skeleton setup of set_<object> in ShippingApiController

--- app/controllers/shipping_api_controller.rb
+++ app/controllers/shipping_api_controller.rb
@@ -4,4 +4,19 @@ class ShippingApiController < ApplicationController
   def rates
     render json: {}
   end
+
+  private
+
+  def set_origin
+  end
+
+  def set_destination
+  end
+
+  def set_package
+  end
+
+  def set_shipping_rate
+    @shipping_rate = ShippingRate.new(@origin, @destination, @package)
+  end
 end
