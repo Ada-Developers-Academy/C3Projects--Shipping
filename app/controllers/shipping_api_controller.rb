358e39bdcd034ea9229aac19e52bf30bb12c36a8
Ashley Watkins <AWatkins31@gmail.com>
2015-08-19 14:04:38 -0700

    Initial setup of ShippingApiController#rates.
    We must skip verify_authenticity_token before the post because we haven't covered how to authenticate on an API.
    We know that a POST method here is breaking RESTful routing, but we decided that the amount of information included justified this.

--- app/controllers/shipping_api_controller.rb
+++ app/controllers/shipping_api_controller.rb
@@ -1,2 +1,7 @@
 class ShippingApiController < ApplicationController
+  skip_before_filter :verify_authenticity_token, only: :rates
+
+  def rates
+    render json: {}
+  end
 end
--- spec/controllers/shipping_api_controller_spec.rb
+++ spec/controllers/shipping_api_controller_spec.rb
@@ -2,4 +2,37 @@ require 'rails_helper'
 
 RSpec.describe ShippingApiController, type: :controller do
 
+  describe "POST rate_compare" do
+    let(:params) {
+      { origin: { zip: "98101", city: "Seattle", state: "WA", country: "US"},
+        destination: { city: "Beaverton", state: "OR", zip: 97005, country: "USA" },
+        package: { weight: 12, dimensions: [15, 10, 4.5], units: :imperial }
+      }
+    }
+
+    before :each do
+      post :rates, params
+    end
+
+    context "when params are valid" do
+      it "is successful" do
+        expect(response.response_code).to eq(200)
+      end
+
+      it "returns json" do
+        expect(response.header['Content-Type']).to include 'application/json'
+      end
+
+      describe "the returned json object" do
+        before :each do
+          @object = JSON.parse(response.body)
+        end
+
+        it "is an array of hashes"
+
+        it "includes only service, price, and delivery_date"
+      end
+    end
+  end
+
 end
