4492dc0c3f21ef5024a05860863f6bceb79a0de2
Ashley Watkins <AWatkins31@gmail.com>
2015-08-19 17:00:20 -0700

    Adds API logging functionality.

--- app/controllers/shipping_api_controller.rb
+++ app/controllers/shipping_api_controller.rb
@@ -5,6 +5,16 @@ class ShippingApiController < ApplicationController
     render json: {}
   end
 
+  def create_log
+    log = Log.new(log_params)
+
+    if log.save
+      render json: log, status: :ok # 200
+    else
+      render json: log.errors, status: :bad_request # 400
+    end
+  end
+
   private
 
   def set_origin
@@ -18,5 +28,9 @@ class ShippingApiController < ApplicationController
 
   def set_shipping_rate
     @shipping_rate = ShippingRate.new(@origin, @destination, @package)
+  end
+
+  def log_params
+    params.require(:log).permit(:customer, :order_id, :service, :cost, :origin, :destination)
   end
 end
